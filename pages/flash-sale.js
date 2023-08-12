546c6880f2ac20915541cb8b6e8102814d7c4555
dieptv1999 <diep.tv1999@gmail.com>
2023-08-12 10:13:48 +0700

    .

--- pages/flash-sale.js
+++ pages/flash-sale.js
@@ -1,5 +1,30 @@
 import React from "react";
 import FlashSale from "../src/components/FlashSale";
-export default function flashSalePage() {
-  return <FlashSale />;
+import {getInstanceAxios} from "../src/apis/Client";
+import isEmpty from "lodash/isEmpty";
+export default function flashSalePage({newsHot}) {
+  return <FlashSale products={newsHot}/>;
+}
+
+export const getServerSideProps = async () => {
+  try {
+    const client = getInstanceAxios(process.env.NEXT_PUBLIC_BASE_URL);
+
+    const resSell = await client.post('getListSellMore')
+    const newsHot = resSell.data?.listSell;
+
+    if (!newsHot || isEmpty(newsHot)) {
+      return {
+        notFound: true,
+      }
+    }
+
+    return {
+      props: {newsHot},
+    }
+  } catch (e) {
+    return {
+      notFound: true,
+    }
+  }
 }
