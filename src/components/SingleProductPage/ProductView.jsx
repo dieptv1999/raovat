4b34148d648c383f45acf5f9b586f5cb88a739cf
dieptv1999 <diep.tv1999@gmail.com>
2023-09-09 00:04:19 +0700

    .

--- src/components/SingleProductPage/ProductView.jsx
+++ src/components/SingleProductPage/ProductView.jsx
@@ -157,7 +157,7 @@ export default function ProductView({className, reportHandler, product}) {
                                 </g>
                             </svg>
                             <div
-                                className="text-white w-[120px]">{utils.transformPhone(product.contact_phone, showPhone)}</div>
+                                className="text-white w-[130px]">{utils.transformPhone(product.contact_phone, showPhone)}</div>
                             <div
                                 className={'text-white flex-1 text-end'}>{showPhone ? 'Bấm để sao chép' : 'Bấm để hiện số'}</div>
                         </div>
@@ -187,15 +187,15 @@ export default function ProductView({className, reportHandler, product}) {
                             <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none"
                                  xmlns="http://www.w3.org/2000/svg">
                                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                                 <g id="SVGRepo_iconCarrier">
                                     <path
                                         d="M22 12C22 8.22876 22 6.34315 20.8284 5.17157C19.6569 4 17.7712 4 14 4H10C6.22876 4 4.34315 4 3.17157 5.17157C2 6.34315 2 8.22876 2 12C2 15.7712 2 17.6569 3.17157 18.8284C4.34315 20 6.22876 20 10 20H14C17.7712 20 19.6569 20 20.8284 18.8284C21.4816 18.1752 21.7706 17.3001 21.8985 16"
-                                        stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path>
-                                    <path d="M10 16H6" stroke="#1C274C" stroke-width="1.5"
-                                          stroke-linecap="round"></path>
-                                    <path d="M14 16H12.5" stroke="#1C274C" stroke-width="1.5"
-                                          stroke-linecap="round"></path>
+                                        stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"></path>
+                                    <path d="M10 16H6" stroke="#1C274C" strokeWidth="1.5"
+                                          strokeLinecap="round"></path>
+                                    <path d="M14 16H12.5" stroke="#1C274C" strokeWidth="1.5"
+                                          strokeLinecap="round"></path>
                                     <path d="M2 10L7 10M22 10L11 10" stroke="#1C274C" strokeWidth="1.5"
                                           strokeLinecap="round"></path>
                                 </g>
